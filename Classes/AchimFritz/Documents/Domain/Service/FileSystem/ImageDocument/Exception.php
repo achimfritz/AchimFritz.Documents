79d9766f8fce9bb3febff7a004130e9f63172ad5
Achim Fritz <af@lightwerk.com>
2015-05-17 16:19:21 +0200

    [DEV] add Exception

---
+++ Classes/AchimFritz/Documents/Domain/Service/FileSystem/ImageDocument/Exception.php
@@ -0,0 +1,10 @@
+<?php
+namespace AchimFritz\Documents\Domain\Service\FileSystem\ImageDocument;
+
+/*                                                                        *
+ * This script belongs to the TYPO3 Flow package "AchimFritz\Documents". *
+ *                                                                        *
+ *                                                                        */
+
+class Exception extends \AchimFritz\Documents\Domain\Service\FileSystem\Exception {
+}
