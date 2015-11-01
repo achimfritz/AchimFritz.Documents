1ae4feab1f9d7c073b55fd5a25d098eb42bd1f08
Achim Fritz <af@lightwerk.com>
2015-11-01 15:59:13 +0100

    [TASK] introduce ApiController

---
+++ Resources/Private/Resources-1.4/JavaScript/Components/Core/ApiController.js
@@ -0,0 +1,100 @@
+/* global angular */
+(function () {
+    'use strict';
+    angular
+        .module('achimfritz.core')
+        .controller('ApiController', ApiController);
+
+    /* @ngInject */
+    function ApiController(FlashMessageService, ExportRestService, DocumentListRestService, DocumentCollectionRestService, CategoryRestService) {
+
+        var vm = this;
+
+        vm.finished = true;
+
+        // used by the view
+        vm.pdfDownload = pdfDownload;
+        vm.zipDownload = zipDownload;
+        vm.listMerge = listMerge;
+        vm.listRemove = listRemove;
+        vm.categoryRemove = categoryRemove;
+        vm.categoryMerge = categoryMerge;
+        vm.renameCategory = renameCategory;
+
+        // not used by the view
+        vm.initController = initController;
+        vm.restSuccess = restSuccess;
+        vm.restError = restError;
+
+        vm.initController();
+
+        function initController() {
+
+        }
+
+        function categoryMerge(category, docs) {
+            vm.finished = false;
+            DocumentCollectionRestService.merge(category, docs).then(vm.restSuccess, vm.restError);
+        }
+
+        function categoryRemove(category, docs) {
+            vm.finished = false;
+            DocumentCollectionRestService.remove(category, docs).then(vm.restSuccess, vm.restError);
+        }
+
+        function listRemove(category, docs) {
+            vm.finished = false;
+            DocumentListRestService.remove(category, docs).then(vm.restSuccess, vm.restError);
+        }
+
+        function listMerge(category, docs) {
+            vm.finished = false;
+            DocumentListRestService.merge(category, docs).then(vm.restSuccess, vm.restError);
+        }
+
+        function zipDownload(zip, docs) {
+            console.log('dsfds');
+            vm.finished = false;
+            ExportRestService.zipDownload(zip, docs).then(
+                function (data) {
+                    vm.finished = true;
+                    var blob = new Blob([data.data], {
+                        type: 'application/zip'
+                    });
+                    saveAs(blob, zip.name + '.zip');
+                },
+                vm.restError
+            );
+        }
+
+        function pdfDownload(pdf, docs) {
+            vm.finished = false;
+            ExportRestService.pdfDownload(pdf, docs).then(
+                function (data) {
+                    vm.finished = true;
+                    var blob = new Blob([data.data], {
+                        type: 'application/pdf'
+                    });
+                    saveAs(blob, 'out.pdf');
+                },
+                vm.restError
+            );
+        }
+
+        function renameCategory(renameCategory) {
+            vm.finished = false;
+            CategoryRestService.update(renameCategory).then(vm.restSuccess, vm.restError);
+        }
+
+        function restSuccess(data) {
+            vm.finished = true;
+            FlashMessageService.show(data.data.flashMessages);
+        }
+
+        function restError(data) {
+            vm.finished = true;
+            FlashMessageService.error(data);
+        }
+
+    }
+})();
--- Resources/Private/Resources-1.4/JavaScript/Components/Image/ClipboardController.js
+++ Resources/Private/Resources-1.4/JavaScript/Components/Image/ClipboardController.js
@@ -14,22 +14,13 @@
         vm.category = '';
         vm.zip = {};
         vm.pdf = {};
-        vm.finished = true;
 
 
         // used by the view
         vm.showForm = showForm;
-        vm.pdfDownload = pdfDownload;
-        vm.zipDownload = zipDownload;
-        vm.listMerge = listMerge;
-        vm.listRemove = listRemove;
-        vm.categoryRemove = categoryRemove;
-        vm.categoryMerge = categoryMerge;
 
         // not used by the view
         vm.initController = initController;
-        vm.restSuccess = restSuccess;
-        vm.restError = restError;
 
         vm.initController();
 
@@ -38,66 +29,8 @@
             vm.zip = ExportRestService.zip();
         }
 
-        function categoryMerge() {
-            vm.finished = false;
-            DocumentCollectionRestService.merge(vm.category, vm.docs).then(vm.restSuccess, vm.restError);
-        }
-
-        function categoryRemove() {
-            vm.finished = false;
-            DocumentCollectionRestService.remove(vm.category, vm.docs).then(vm.restSuccess, vm.restError);
-        }
-
-        function listRemove() {
-            vm.finished = false;
-            DocumentListRestService.remove(vm.category, vm.docs).then(vm.restSuccess, vm.restError);
-        }
-
-        function listMerge() {
-            vm.finished = false;
-            DocumentListRestService.merge(vm.category, vm.docs).then(vm.restSuccess, vm.restError);
-        }
-
-        function zipDownload() {
-            vm.finished = false;
-            ExportRestService.zipDownload(vm.zip, vm.docs).then(
-                function (data) {
-                    vm.finished = true;
-                    var blob = new Blob([data.data], {
-                        type: 'application/zip'
-                    });
-                    saveAs(blob, vm.zip.name + '.zip');
-                },
-                vm.restError
-            );
-        }
-
-        function pdfDownload() {
-            vm.finished = false;
-            ExportRestService.pdfDownload(vm.pdf, vm.docs).then(
-                function (data) {
-                    vm.finished = true;
-                    var blob = new Blob([data.data], {
-                        type: 'application/pdf'
-                    });
-                    saveAs(blob, 'out.pdf');
-                },
-                vm.restError
-            );
-        }
-
         function showForm(form) {
             vm.form = form;
-        }
-
-        function restSuccess(data) {
-            vm.finished = true;
-            FlashMessageService.show(data.data.flashMessages);
-        }
-
-        function restError(data) {
-            vm.finished = true;
-            FlashMessageService.error(data);
         }
 
         $rootScope.$on('docsUpdate', function (event, docs) {
