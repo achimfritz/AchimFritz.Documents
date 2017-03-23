df479e4809e64b09252ddacd08eb1785ef453f96
Achim Fritz <af@achimfritz.de>
2017-03-23 18:59:27 +0100

    [TASK] mp3 (ui stuff)

--- Resources/Private/Resources/JavaScript/Components/Music/Controller/Navigation.js
+++ Resources/Private/Resources/JavaScript/Components/Music/Controller/Navigation.js
@@ -19,8 +19,7 @@
             {name: 'playlists', location: 'music/list'},
             {name: 'filter', location: 'music/filter'},
             {name: 'player', location: 'music/player'},
-            {name: 'docList', location: 'music/docList'},
-            {name: 'ui', location: 'music/ui'}
+            {name: 'docList', location: 'music/docList'}
         ];
         vm.forward = forward;
 
